9395c26c021dcff2dd4be6084e680e276a92f9a5
bettariraju741 <155747843+bettariraju741@users.noreply.github.com>
2026-06-20 11:30:50 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "this" {
-  ami                    = "ami-09c813fb71547fc4f" # This is our devops-practice AMI ID
+  ami                    = "ami-0521cb2d60cfbb1a6" # This is our devops-practice AMI ID
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
   instance_type          = "t3.micro"
   tags = {
